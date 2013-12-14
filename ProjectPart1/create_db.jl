de7013ba761beea6cedfcae4a1831fb7049d9851
Iain Dunning <iaindunning@gmail.com>
2013-12-14 18:22:18 -0500

    Files to create and test the stations SQLite database

---
+++ ProjectPart1/create_db.jl
@@ -0,0 +1,6 @@
+using SQLite
+
+db = SQLite.connect("stations.sqlite")
+createtable("../Hubway/stations.csv", db, name="stations", 
+            delim=',', header=true)
+close(db)
---
+++ ProjectPart1/test_db.jl
@@ -0,0 +1,9 @@
+using SQLite
+
+db = SQLite.connect("stations.sqlite")
+result = query("SELECT * FROM stations WHERE name LIKE '%MIT%'")
+println("MIT Stations")
+for i in 1:size(result,1)
+    println("#$(i) [ID $(result[i,:id])]: $(result[i,:name])")
+end
+close(db)
