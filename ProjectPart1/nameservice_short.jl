048b84a34fc0921deaf7b938afe2050c972afc10
Iain Dunning <iaindunning@gmail.com>
2013-12-14 16:59:18 -0500

    Added terse version of nameserver

---
+++ ProjectPart1/nameservice_short.jl
@@ -0,0 +1,24 @@
+#############################################################################
+# MIT 15.S60
+# Software Tools for Operations Research
+# IAP 2014
+#############################################################################
+# NameService
+# A simple web service that, when given a name, inserts the name into
+# a message and returns it back to the user
+#
+# This is the short version of "solution" file.
+#############################################################################
+
+using HttpServer
+function nameservice(req::Request, res::Response)
+    if ismatch(r"/nameservice/", req.resource)
+        req_split = split(req.resource, "/")
+        if length(req_split) != 3
+            return Response(400, "Error: too many arguments!")
+        end
+        return Response("<blink>Hey there $(req_split[3])!</blink>")
+    end
+    return Response(404, "We don't do that...")
+end
+run(Server(HttpHandler(nameservice)), 8000)
