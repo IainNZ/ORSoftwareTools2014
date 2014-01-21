45a3561ec353297625acd0e0abc23f4cc039ba84
Ross Anderson <rma350@gmail.com>
2014-01-21 16:53:02 -0500

    Fixed things in README, added Winston version of project.

--- ProjectPart2/README.md
+++ ProjectPart2/README.md
@@ -6,11 +6,10 @@ The lecture notes are a pdf in the github repository.
 
 ## Installation Instructions (Recommended)
 
-Please install the Winston package (see [GitHub](https://github.com/nolta/Winston.jl) and [these instructions](http://homerreid.dyndns.org/teaching/18.330/InstallingWinston.shtml))
+Please install the Winston package (see [GitHub](https://github.com/nolta/Winston.jl) and [these instructions](http://homerreid.dyndns.org/teaching/18.330/InstallingWinston.shtml))/
 
 Winston is a graphics package for Julia.  If you are unable to install
-Winston, there is an alternate (less cool) version of the project you
-can still make that uses text instead of graphics.
+Winston, there is an alternate (less cool) version of the project that uses text instead of graphics.
 
 Be sure that you have Julia and Gurobi working (i.e. that you completed the installation assignment for the [Modeling class](https://github.com/IainNZ/ORSoftwareTools2014/tree/master/Modelling).
 
@@ -18,21 +17,23 @@ Be sure that you have Julia and Gurobi working (i.e. that you completed the inst
 
 We have provided an _enhanced solution_ to Project Part I, which we will be using in Part II.  Make sure you can run it!  We recommend you make a fresh start from Part I in a new directory, but you will have to repeat a few of the installation steps.  We have provided duplicates of a few scripts.
 
-1. (Repeated from last time) Run `create_db.jl` to build the SQLite
+* (Repeated from last time) Run `create_db.jl` to build the SQLite
 database.  You should see a new file `stations.sqlite` in the working
 directory.
-2. (Repeated from last time) Run `test_db.jl` to make sure
+* (Repeated from last time) Run `test_db.jl` to make sure
 it is working.
 
 ### If you were able to install Winston
 
-3. Run `tsp_service_winston.jl`. You should get a message like "Listening on 8000..."
-4. Open a browser and to go to [http://localhost:8000/stationservice/42.3/42.4/-71.2/-71.0].  You should see a plot of the hubway stations on the x-y plane connected in a tour.
+* Run `tsp_service_winston.jl`. You should get a message like "Listening on 8000..."
+* Open a browser and to go to http://localhost:8000/stationservice/42.3/42.4/-71.2/-71.0.  You should see a plot of the hubway stations on the x-y plane connected in a tour.  It should like this:
+
+![Alt text](init-tour.png "Initial output for tsp_service_winston.jl")
 
 ### If you couldn't install Winston
 
-3. Run `tsp_service_text.jl`. You should get a message like "Listening on 8000..."
-4. Open a browser and to go to [http://localhost:8000/stationservice/42.3/42.4/-71.2/-71.0].  You should see a list of cities.
+* Run `tsp_service_text.jl`. You should get a message like "Listening on 8000..."
+* Open a browser and to go to http://localhost:8000/stationservice/42.3/42.4/-71.2/-71.0 .  You should see a list of hubway station names.
 
 
 ### Submission
---
+++ ProjectPart2/tsp_service_winston.jl
@@ -0,0 +1,119 @@
+#############################################################################
+# MIT 15.S60
+# Software Tools for Operations Research
+# IAP 2014
+#############################################################################
+# StationService
+# A webservice that takes four lat-lon points and returns the names of all
+# stations inside that box.
+#############################################################################
+
+using HttpServer
+using SQLite
+
+# Provides the type FileResponse
+using HttpCommon
+# Uncomment out when the TSP solver is ready
+include("tspSolver.jl")
+
+
+# Create, load the database
+SQLite.connect("hubway.sqlite")
+
+function tspservice(req::Request, res::Response)
+    println(req.resource)
+    
+    if ismatch(r"/stationservice/", req.resource)
+        req_split = split(req.resource, "/")
+        println(req_split)
+
+        # We are expecting the length of req_split to be 6:
+        # * Before the first slash (empty string)
+        # * stationservice
+        # * latL
+        # * latR
+        # * lonL
+        # * lonR
+        if length(req_split) != 6
+            return Response(400, "Error: wrong number of arguments!")
+        end
+
+        # Attempt to parse the arguments
+        latL = 0.0
+        latR = 0.0
+        lngL = 0.0
+        lngR = 0.0
+        try
+            latL = float(req_split[3])
+            latR = float(req_split[4])
+            lngL = float(req_split[5])
+            lngR = float(req_split[6])
+        catch
+            return Response(400, "Error: couldn't parse arguments!")
+        end
+
+        # Build SQL query
+        sql_query = "SELECT name, lat, lng FROM stations WHERE ( lat >= $latL AND lat <= $latR ) AND ( lng >= $lngL AND lng <= $lngR )"
+
+        # Run the query
+        results = query(sql_query)
+        println(results)
+
+        # Return results
+        N = size(results, 1)
+        if N == 0
+            return Response(400, "No results")
+        else
+            # If we want to just return the stations
+            #ret_str = "$(size(results,1)) results:"
+            #for i in 1:size(results, 1)
+            #    ret_str = string(ret_str, "<br>$(results[i,:name])")
+            #end
+            #return Response(ret_str)
+            latlngs = zeros(N, 2)
+            for i in 1:N
+                latlngs[i,1] = float(results[i,:lat])
+                latlngs[i,2] = float(results[i,:lng])
+            end
+            mat = getDistMatrix(latlngs)
+            (optDist,tour) = solveTsp(mat)
+            println(optDist)
+            plotTour(tour,latlngs,"tour.png")
+            return FileResponse("tour.png")
+            #ret_str = string(ret_str,tour)
+            #return Response(ret_str)
+        end
+
+    else
+        # User requested something that we don't do!
+        # Return 404 NOT FOUND
+        return Response(404, "We don't do that...")
+    end
+end
+
+# Computes the approximate distance between two points rounded to the nearest
+# meter. Assumes that the points are near a latitude of 40 degrees.
+function distance(lat1, lng1, lat2, lng2)
+    yDist = 111000*(lat2 - lat1)
+    xDist = 85000*(lng2 - lng1)
+    return iround(sqrt(yDist^2 + xDist^2))
+end
+
+# Input: an n x 2 matrix of latitude longitude pairs.
+# Output a symmetric n x n matrix of distances rounded to the nearest meter.
+function getDistMatrix(points)
+    n = size(points,1)
+    return [distance(points[i,1],points[i,2],points[j,1],points[j,2]) for i=1:n, j=1:n]
+end
+
+
+
+# Create and start the server
+http = HttpHandler(tspservice)
+server = Server(http)
+run(server, 8000)
+
+# http://localhost:8000/stationservice/42.34/42.341/-71.11/-71.1
+# 2 results:
+# Colleges of the Fenway
+# Longwood Ave/Riverway
